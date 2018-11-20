f91a7ae8f2fd372fc405b56222135fac90e00bab
rlcoley <rosscoley1002@gmail.com>
2018-11-20 17:52:08 -0500

    added to ross branch

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,2 +1,28 @@
 class BoatsController < ApplicationController
+
+  def index
+      @boats = Boat.all
+  end
+
+  def new
+    @boat = Boat.new
+  end
+
+  def create
+    boat = Boat.new(boat_params)
+    boat.save
+    redirect_to "/boats"
+  
+  end
+
+  # def show
+  #   @boat = Boat.find(params[:id])
+  # end
+
+
+
+  def boat_params
+    params.require(:boat).permit(:boat_name, :container_capacity, :port_of_origin, :user_id)
+  end
+
 end
