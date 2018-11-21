60a6c1afe212cf0e0ee35defcf962058852402f2
SethKalkstein <sethkal7@yahoo.com>
2018-11-21 10:52:09 -0500

    job index.html.erb started and deletion from job model

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,4 @@ class Job < ApplicationRecord
   validates :job_description, length: {minimum: 20}
   validates :job_cost, numericality: { greater_than_or_equal_to: 100,  only_integer: true }
   has_and_belongs_to_many :boats
-  belongs_to :user
 end
